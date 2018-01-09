97851138549b9b910b18f5f037219506c0c92c82
mark philips <mphilips15@gmail.com>
2018-01-09 16:21:59 -0500

    Added all (presumed) app files and Activities. Code to be implemented

---
+++ app/src/main/java/com/example/markphilips/dungeons_and_dragons_companion/BioActivity.java
@@ -0,0 +1,13 @@
+package com.example.markphilips.dungeons_and_dragons_companion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bio);
+    }
+}
---
+++ app/src/main/java/com/example/markphilips/dungeons_and_dragons_companion/model/Spell.java
@@ -0,0 +1,8 @@
+package com.example.markphilips.dungeons_and_dragons_companion.model;
+
+/**
+ * Created by Mark on 1/9/2018.
+ */
+
+public class Spell {
+}
