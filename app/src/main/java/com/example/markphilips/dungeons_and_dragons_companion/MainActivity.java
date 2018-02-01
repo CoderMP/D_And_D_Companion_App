3eb76edf728a876ba32339bde6fa0d7b2a93e5fd
mark philips <mphilips15@gmail.com>
2018-02-01 10:48:14 -0500

    Progressed Implementation of MainActivity back-end by beginning implementation of Activity Navigation. Removed FloatingActionButton.

--- app/src/main/java/com/example/markphilips/dungeons_and_dragons_companion/MainActivity.java
+++ app/src/main/java/com/example/markphilips/dungeons_and_dragons_companion/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.markphilips.dungeons_and_dragons_companion;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,10 +11,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     //region Private Field Variables
@@ -39,34 +40,42 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private Button btnDsFailureIncrement;
     private Button btnDsFailureDecrement;
     private TextView txtDeathSaveFailureTotal;
+
+    private Intent navIntent;
     //endregion
 
+    //region Creation Methods
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //TODO: Open SQLiteDatabases for Character, Weapons, Armour, & Spells & load in the appropriate data
+
+        //Call the setupViews method
+        setupViews();
     }
 
+
+    /**
+     * Method that is responsible for setting up the necessary Views for the MainActivity
+     *
+     * @author Mark Philips
+     * @since 01/30/2018
+     * @version 1
+     */
     private void setupViews(){
         edtCharName = findViewById(R.id.edt_char_name);
         edtArmourRating = findViewById(R.id.edt_armour_rating);
@@ -90,6 +99,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         txtDeathSaveSuccessTotal = findViewById(R.id.txt_death_save_success_total);
         txtDeathSaveFailureTotal = findViewById(R.id.txt_death_save_failure_total);
 
+        //region Button OnClickListeners
         btnDsSuccessIncrement.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -102,30 +112,73 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 successIncrementDecrement(v);
             }
         });
+        btnDsFailureIncrement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                successIncrementDecrement(v);
+            }
+        });
+        btnDsFailureDecrement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                successIncrementDecrement(v);
+            }
+        });
+        //endregion
+
+        //region Spinner OnItemSelectedListeners
+        spnCharRace.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        spnCharClass.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //TODO:Implement Methods
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        spnCharBackground.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //TODO:Implement Methods
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        spnCharAlignment.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //TODO:Implement Methods
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        //endregion
     }
 
-
-    /**
-     * Private method that is responsible for incrementing or decrementing the Death Save Success
-     * total.
-     *
-     * @param view The increment/decrement button that was tapped
-     */
-    private void successIncrementDecrement(View view){
-        //Retrieve the ID of the View that was passed to the method
-        int viewID = view.getId();
-
-        if (viewID == btnDsSuccessIncrement.getId()){
-            //TODO: Implement code to increment & update total
-        }
-        else if (viewID == btnDsSuccessDecrement.getId()){
-            //TODO: Implement code to decrement & update total
-        }
-    }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -133,6 +186,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
+
+    /**
+     * Method that is responsible for inflating the ActionBar from the according XML layout file
+     *
+     * @param menu The ActionBar Object that needs to be inflated
+     * @return
+     *
+     * @author Mark Philips
+     * @since 01/02/2018
+     * @version 1
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -154,29 +218,91 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         return super.onOptionsItemSelected(item);
     }
+    //endregion
 
-    @SuppressWarnings("StatementWithEmptyBody")
+    //region Navigation Method(s)
+    /**
+     * Method that is responsible for handling inter-Activity navigation based on the tapped menu
+     * items in the Navigation Drawer
+     *
+     * @param item The MenuItem that was tapped
+     * @return
+     *
+     * @author Mark Philips
+     * @since 01/02/2018
+     * @version 1
+     */
+    @SuppressWarnings("StatementWithEmptyBody") //TODO: Remove this statement once navigation cases are complete
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+        //Get the ID of the MenuItem that was tapped in the Navigation Drawer
         int id = item.getItemId();
 
-//        if (id == R.id.nav_camera) {
-//            // Handle the camera action
-//        } else if (id == R.id.nav_gallery) {
-//
-//        } else if (id == R.id.nav_slideshow) {
-//
-//        } else if (id == R.id.nav_manage) {
-//
-//        } else if (id == R.id.nav_share) {
-//
-//        } else if (id == R.id.nav_send) {
-//
-//        }
+        //Initialize the navIntent with the appropriate Activity.class naming and Context, then navigate
+        if (id == R.id.nav_stats) {
+            navIntent = new Intent(getApplicationContext(), StatsActivity.class);
+            startActivity(navIntent);
+        }
+        else if (id == R.id.nav_bio) {
+            navIntent = new Intent(getApplicationContext(), BioActivity.class);
+            startActivity(navIntent);
+        }
+        else if (id == R.id.nav_spells) {
+            navIntent = new Intent(getApplicationContext(), SpellsActivity.class);
+            startActivity(navIntent);
+        }
+        else if (id == R.id.nav_inventory) {
+            navIntent = new Intent(getApplicationContext(), InventoryActivity.class);
+            startActivity(navIntent);
+        }
+        else if (id == R.id.nav_info_races) {
+            //TODO: Implement InfoRacesActivity navigation case
+            //navIntent = new Intent(getApplicationContext(), InfoRacesActivity.class);
+            //startActivity(navIntent);
+        }
+        else if (id == R.id.nav_info_classes) {
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+    //endregion
+
+    //region Methods
+    /**
+     * Private method that is responsible for incrementing or decrementing the Death Save Success
+     * total.
+     *
+     * @param view The increment/decrement button that was tapped
+     *
+     * @author Mark Philips
+     * @since 01/25/2018
+     * @version 1
+     */
+    private void successIncrementDecrement(View view){
+        //Retrieve the ID of the View that was passed to the method
+        int viewID = view.getId();
+
+        //Determine which button was tapped and manipulate the data accordingly
+        if (viewID == btnDsSuccessIncrement.getId()){
+            //TODO: Implement code to increment & update total
+            Toast.makeText(this, "Success Incr Button Tapped", Toast.LENGTH_SHORT).show();
+
+        }
+        else if (viewID == btnDsSuccessDecrement.getId()){
+            //TODO: Implement code to decrement & update total
+            Toast.makeText(this, "Success Decr Button Tapped", Toast.LENGTH_SHORT).show();
+        }
+        else if (viewID == btnDsFailureIncrement.getId()){
+            //TODO: Implement code to increment & update total
+            Toast.makeText(this, "Failure Incr Button Tapped", Toast.LENGTH_SHORT).show();
+        }
+        else if (viewID == btnDsSuccessDecrement.getId()){
+            //TODO: Implement code to decrement & update total
+            Toast.makeText(this, "Failure Decr Button Tapped", Toast.LENGTH_SHORT).show();
+        }
+    }
+    //endregion
 }
